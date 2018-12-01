8b0917c52490e6d79475cbda7c61aee4cb6d5676
omarsgalal <omarsgalal4@gmail.com>
2018-12-01 19:28:35 +0200

    for pulling -__-

--- SkinModel/SkinModel.py
+++ SkinModel/SkinModel.py
@@ -3,7 +3,9 @@ import cv2
 import skimage.io as io
 import os 
 from skimage.color import rgb2gray,rgb2hsv,hsv2rgb
-from TrainSkinModel import TrainSkinModel
+import sys
+sys.path.append('../')
+from SkinModel.TrainSkinModel import TrainSkinModel
 
 # skinHisto = np.zeros((256,256,256))
 # nonskinHisto = np.zeros((256,256,256))
@@ -18,9 +20,6 @@ class SkinModel():
         
         self.skinHistoSum = np.sum(self.skinHisto,axis=2)
         self.nonskinHistoSum = np.sum(self.nonskinHisto,axis=2)
-
-        self.lower = np.array([5, 5, 80], dtype = "uint8")
-        self.upper = np.array([50, 175, 250], dtype = "uint8")
 
         
     def getModels(self):
@@ -72,102 +71,21 @@ class SkinModel():
         mask1 = (mask1 > 0.15) * (mask1 < 1.1)
         mask2 = (mask2 > -4) * (mask2 < 0.3)
 
-        mask = (mask1 * mask2 ).astype('uint8')
+        mask = (mask1 * mask2).astype('uint8')
         return mask
 
 
-
-    def detectRange(self,img,mode='BGR'):
-        if mode == 'BGR':
-            img = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_BGR2HSV)
-        elif mode=='RGB':
-            img = rgb2hsv(img)
-        elif mode=='HSV':
-            pass
-        else:
-            raise ValueError('Image mode is not RGB nor HSV')
-
-        skinMask = cv2.inRange(img, self.lower, self.upper)
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-        skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
-        skinMask = cv2.erode(skinMask, kernel, iterations = 2)
-        # skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0)
-        # print(np.max(skinMask),np.min(skinMask))
-        return (skinMask/255.0).astype('uint8')
-
-    def detectRangeAllSpaces (self,img,mode='BGR'):
-        """https://arxiv.org/pdf/1708.02694.pdf"""
-        """https://www.researchgate.net/publication/26593885_A_Skin_Detection_Approach_Based_on_Color_Distance_Map"""
-        if mode == 'BGR':
-            rgb = img[...,::-1]
-            hsv = cv2.cvtColor(img.astype('uint8'), cv2.COLOR_BGR2HSV)
-        elif mode=='RGB':
-            rgb = img
-            hsv = rgb2hsv(img)
-        elif mode=='HSV':
-            rgb = hsv2rgb(img)
-            hsv = img
-        else:
-            raise ValueError('Image mode is not RGB nor HSV')
-
-        lowerHSV = np.array([0, 0, 0], dtype = "uint8")
-        upperHSV = np.array([50, 175, 255], dtype = "uint8")
-
-        lowerRGB = np.array([95, 40, 20], dtype = "uint8")
-        upperRGB = np.array([255, 255, 255], dtype = "uint8")
-
-        mask1 = cv2.inRange(hsv, lowerHSV, upperHSV)
-        mask2 = cv2.inRange(rgb, lowerRGB, upperRGB)
-        #  R > G and R > B and | R - G | > 15 and A > 15
-        mask3 = (rgb[:,:,0] > rgb[:,:,1]) * (rgb[:,:,0] > rgb[:,:,2]) * ((rgb[:,:,0] - rgb[:,:,1]) > 15)
-
-        skinMask =  mask1 * mask2 * mask3
-
-        kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-        skinMask = cv2.dilate(skinMask, kernel, iterations = 2)
-        skinMask = cv2.erode(skinMask, kernel, iterations = 2)
-        return skinMask
-        
-
-
-
+    
 
 def main():
+    from VideoSequence import VideoSequence as Vs
+    from utils import showImages
     s = SkinModel()
-    # pskin = s.skinHisto *1.0 / s.Tskin  + 0.00000000001
-    # pnon = s.nonskinHisto*1.0 / s.Tnon  + 0.000001
-    # # for x in range(100):
-    # x = 1
-    # mask = np.array(pnon/pskin < x,dtype=np.uint8)
-    # obj = ndimage.find_objects(mask)
-    # if obj:
-    #     print(mask[obj[0]].shape)
-    #     print(obj,np.max(mask))
-
-
-    # resultImageDiff, _ = ndimage.measurements.label(mask)
-    # objs = ndimage.find_objects(resultImageDiff)
-
-    # max_area = 0
-    # mROI=[0,0,0,0]
-    # for obj in objs:
-    #     area = (obj[0].start - obj[0].stop ) * (obj[1].start - obj[1].stop) 
-    #     if area > max_area:
-    #         max_area = area
-    #         # mROI = [obj[0].start, obj[0].stop, obj[1].start, obj[1].stop]#ymin,ymax,xmin,xmax
-    #         mROI = obj
-
-    # print (mROI)
-
-
-    vs = cv2.VideoCapture(0)
+    vs = Vs().start()
     while(True):
-        # frames = vs.process()
-        img = vs.read()[1]
-        # frames = [s.detect(img,x) for x in range(0,1,0.1)]
-        # for x in range(10):
-        frame = s.detectRangeAllSpaces(img)
-        cv2.imshow("x= {}",cv2.bitwise_and(img,img,mask = frame))
+        frames = vs.process()
+        img = s.detect(frames("BGR")[-2])
+        showImages([img*255], ["skin detector Model only"])
         if cv2.waitKey(1) == 27: 
             break  # esc to quit
 if __name__ == "__main__":
